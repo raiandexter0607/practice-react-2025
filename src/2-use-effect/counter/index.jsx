2daae31c8118335fdfa4883d52d040a14932ec0a
Brayan Duque <bduque@lean-tech.io>
2025-03-11 11:56:19 -0500

    feat(use-effect): adding format

--- src/2-use-effect/counter/index.jsx
+++ src/2-use-effect/counter/index.jsx
@@ -4,13 +4,19 @@ const Counter = () => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
+
+    //callback to execute
     const intervalId = setInterval(() => {
       setCounter((count) => (count += 1));
     }, 1000);
+
+    // cleaner function
     return () => {
       clearInterval(intervalId);
     };
-  }, []);
+  }, []); // dependencies
+
+  
   return (
     <>
       <h1>{counter}</h1>
